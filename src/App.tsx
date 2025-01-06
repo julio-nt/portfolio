7f9c4771619d1987271704aaea429448d8c13637
julio-nt <julio.nt753@gmail.com>
2025-01-06 19:11:34 -0300

    background absolute

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import Presentation from "./components/Presentation/Presentation";
 function App() {
   return (
     <>
-      <div className="top-container">
+      <div className="absolute top-container">
         <h1 className="top-title">{"< Júlio Nunes Tavares />"}</h1>
       </div>
+      <div className="first-section" />
       <Presentation />
     </>
   );
